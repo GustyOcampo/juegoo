9f4ca931bc898cf32e558044cf5366cc8cb0dce1
GustyOcampo <gusuel581@gmail.com>
2023-05-31 16:00:29 -0300

    agregamos las parte logica

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-console.log ("Hola Mundo")
+/*console.log ("Hola Mundo")
+console.log (25+5)
+console.log ("Hola la temperatura de hoy es:",-10, "°C")*/
+
+const letrasContainer=document.getElementById("letras-container");
+const opcionesContainer=documment.getElementById("opciones-container");
+const seccionIngresarUsuario=document.getElementById("seccion-ingresar-usuario");
+const nuevoJuegoContainer=document.getElementsByClassName("nuevo-juego-container");
+const nuevoJuegoButton=document.getElementById("nuevo-juego-button");
+const canvas=document.getElementById("canvas");
+const resultadoTexto=document.getElementById("resultado-texto");
+
+let opciones={
+    frutas: [
+        "Manzana",
+        "Frutilla",
+        "Pera",
+        "Sandia",
+        "Naranja",
+        "Mandarina",
+        "Limon",
+        "Uva",
+    ],
+
+    animales: [
+        "Perro",
+        "Gato",
+        "Hamster",
+        "Nutria",
+        "Jirafa",
+        "Leon",
+        "Pantera",
+        "Rinoceronte",
+    ],
+
+    paises: [
+        "Argentina",
+        "Brasil",
+        "Alemania",
+        "Estados Unidos",
+        "España",
+        "Suiza",
+        "Africa",
+        "Irlanda"
+    ],
+}
